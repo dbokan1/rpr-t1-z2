133455be6e66a3522ebf2093346278c046687f4f
Davor <dbokan1@etf.unsa.ba>
2020-10-30 16:57:30 +0100

    initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,29 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static int sumaCifara(int n)
+    {
+        int suma=0;
+        while(n>0)
+        {
+            suma=suma+n%10;
+            n=n/10;
+        }
+        return suma;
+    }
+
+    public static void main(String[] args) {
+	Scanner ulaz=new Scanner(System.in);
+	int n;
+	n=ulaz.nextInt();
+
+	for(int i=1;i<=n;i++)
+    {
+        if(i%sumaCifara(i)==0) System.out.println(i);
+    }
+
+    }
+}
